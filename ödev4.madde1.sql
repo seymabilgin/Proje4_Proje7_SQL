ec10039f01e1e0860b8e91574510668d4342e23d
Şeyma Bilgin <149247597+seymabilgin@users.noreply.github.com>
2024-07-30 11:02:02 +0300

    Update ödev4.madde1.sql

--- ödev4.madde1.sql
+++ ödev4.madde1.sql
@@ -1,4 +1,4 @@
-//Film tablosunda bulunan replacement_cost sütununda birbirinden farklı değerleri sırala
+--Film tablosunda bulunan replacement_cost sütununda birbirinden farklı değerleri sırala
 SELECT DISTINCT replacement_cost
 FROM film
 ORDER BY replacement_cost;
