a6b13cc91ee35550315ca5892b93af33a0564947
Şeyma Bilgin <149247597+seymabilgin@users.noreply.github.com>
2024-07-30 11:02:24 +0300

    Update ödev4.madde3.sql

--- ödev4.madde3.sql
+++ ödev4.madde3.sql
@@ -1,3 +1,4 @@
+--film tablosunda bulunan film isimlerinde (title) kaç tanesini T karakteri ile başlar ve aynı zamanda rating 'G' ye eşittir?
 SELECT COUNT(*)title
 FROM film
 WHERE title LIKE 'T%' AND rating = 'G';
